ae7c2fc01127a525a722694a0adff003d2a2b08d
Amrith Nayak <amrith92@gmail.com>
2025-10-25 13:28:13 +0530

    Fix ConfigLoader to return compiled policy set

--- api/src/main/java/io/fluxgate/api/ConfigLoader.java
+++ api/src/main/java/io/fluxgate/api/ConfigLoader.java
@@ -1,11 +1,10 @@
 package io.fluxgate.api;
 
-import io.fluxgate.core.policy.LimitPolicy;
+import io.fluxgate.core.policy.CompiledPolicySet;
 import io.fluxgate.core.policy.PolicyCompiler;
 
 import java.io.InputStream;
 import java.nio.file.Path;
-import java.util.Collection;
 
 public final class ConfigLoader {
 
@@ -13,11 +12,11 @@ public final class ConfigLoader {
         throw new IllegalStateException("Cannot instantiate " + getClass());
     }
 
-    public static Collection<LimitPolicy> load(Path path) {
+    public static CompiledPolicySet load(Path path) {
         return PolicyCompiler.fromYaml(path);
     }
 
-    public static Collection<LimitPolicy> load(InputStream inputStream) {
+    public static CompiledPolicySet load(InputStream inputStream) {
         return PolicyCompiler.fromYaml(inputStream);
     }
 }
